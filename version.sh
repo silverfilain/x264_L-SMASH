8c73ad967fb9b24b91797b1b0baa118de6b02ad0
Takashi Hirata <silverfilain@gmail.com>
2010-08-26 18:40:42 +0900

    Fix version.sh for this unofficial repo.

--- version.sh
+++ version.sh
@@ -2,7 +2,7 @@
 git rev-list HEAD | sort > config.git-hash
 LOCALVER=`wc -l config.git-hash | awk '{print $1}'`
 if [ $LOCALVER \> 1 ] ; then
-    VER=`git rev-list origin/master | sort | join config.git-hash - | wc -l | awk '{print $1}'`
+    VER=`git rev-list porigin/master | sort | join config.git-hash - | wc -l | awk '{print $1}'`
     if [ $VER != $LOCALVER ] ; then
         VER="$VER+$(($LOCALVER-$VER))"
     fi
