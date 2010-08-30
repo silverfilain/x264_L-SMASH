722fb0f9678defab7316ba5ff16f92b3f8ce8046
Takashi Hirata <silverfilain@gmail.com>
2010-08-30 20:55:02 +0900

    Fix some author information.

--- output/mp4.c
+++ output/mp4.c
@@ -6,6 +6,7 @@
  * Authors: Laurent Aimar <fenrir@via.ecp.fr>
  *          Loren Merritt <lorenm@u.washington.edu>
  *          Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
+ *          Takashi Hirata <silverfilain@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
