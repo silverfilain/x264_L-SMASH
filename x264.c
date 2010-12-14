44b5165359cc86f955fbb047147685cd8c958b0e
Takashi Hirata <silverfilain@gmail.com>
2010-12-15 06:19:44 +0900

    Add --no-remux to the help.
    Also fix overflow of 80 columns.

--- x264.c
+++ x264.c
@@ -796,8 +796,10 @@ static void help( x264_param_t *defaults, int longhelp )
     H2( "      --chapter <string>      Set the chapter list from chapter format file\n"
         "                              Format of each line: hours:minutes:seconds.milliseconds chaptername\n" );
     H2( "      --language <string>     Set the language by ISO639-2 language codes\n" );
-    H2( "      --no-container-sar      Eliminate sample aspect ratio information on the container level\n"
-        "                              Valid only with mp4/3gp/3g2 muxer (otherwise ignored)\n" );
+    H2( "      --no-container-sar      Disable sample aspect ratio within the container\n"
+        "                              Valid only with the mp4/3gp/3g2 muxer\n" );
+    H2( "      --no-remux              Inhibit auto-remuxing for progressive download\n"
+        "                              Valid only with the mp4/3gp/3g2 muxer\n" );
     H0( "\n" );
     H0( "Filtering:\n" );
     H0( "\n" );
